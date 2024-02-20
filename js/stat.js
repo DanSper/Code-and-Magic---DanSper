e3a612a87d1d833454c3332ec72aba0b55cf0c39
Danil <105743321+DanSper@users.noreply.github.com>
2024-02-20 22:41:07 +0400

    make cloud of statisics with gistogramma (#2)
    * make cloud of statisics with gistogramma
    
    * add names to color
    
    ---------
    
    Co-authored-by: DanSper <danilsperantsev@gmail.com>

---
+++ js/stat.js
@@ -0,0 +1,65 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 150;
+var CLOUD_Y = 10;
+var TITLE_HEIGHT = 70;
+var GAP = 10;
+var TEXT_HEIGHT = 15;
+var TEXT_WIDTH = 50;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = CLOUD_HEIGHT - TEXT_HEIGHT - TEXT_HEIGHT - GAP - TITLE_HEIGHT;
+var GREY = 'rgba(0, 0, 0, 0.7)';
+var WHITE = '#fff';
+var BLACK = '#000';
+var RED = 'rgba(255, 0, 0, 1)';
+
+var renderCloud = function(ctx, x, y, color) {
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+	var maxElement = arr[0];
+	
+	for(var i = 0; i < arr.length; i++) {
+		if(arr[i] > maxElement) {
+			maxElement = arr[i];
+		}
+	}
+	
+	return maxElement;
+};
+
+function randomNum(max) {
+	return Math.floor(Math.random() * max);
+};	
+
+window.renderStatistics = function(ctx, names, times) {
+	
+	renderCloud(ctx,  CLOUD_X + GAP, CLOUD_Y + GAP, GREY);
+	renderCloud(ctx, CLOUD_X, CLOUD_Y, WHITE);
+	
+	ctx.fillStyle = BLACK;
+	ctx.font = '16px PT Mono';
+	ctx.fillText('Ура! Вы победили!', CLOUD_X + (GAP) * 2, CLOUD_Y + (TEXT_HEIGHT) * 2);
+	ctx.fillText('Список результатов:', CLOUD_X + (GAP) * 2, CLOUD_Y + (TEXT_HEIGHT) * 3);
+
+	
+	
+	var maxTime = getMaxElement(times);
+	
+	
+	for (var i = 0; i < names.length; i++) {		
+		var currentBarHeight = 	(BAR_HEIGHT * times[i]) / maxTime;
+		var currentDelta = BAR_HEIGHT - currentBarHeight;
+		ctx.fillStyle = BLACK;
+		ctx.fillText(names[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH * 2 + GAP) * i, CLOUD_HEIGHT);
+		ctx.fillText((Math.floor(times[i])), CLOUD_X + BAR_WIDTH + (BAR_WIDTH * 2 + GAP) * i, TITLE_HEIGHT + GAP + currentDelta);
+		ctx.fillStyle = 'rgba(' + randomNum(180) + ',' + randomNum(180) +', 255)';
+		if (names[i] == "Вы") {
+			ctx.fillStyle = RED;
+		}		
+		ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH * 2 + GAP) * i, TITLE_HEIGHT + (GAP) * 2 + currentDelta, BAR_WIDTH, currentBarHeight);		
+	}			
+}; 
+
