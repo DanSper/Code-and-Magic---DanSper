26d6441fd61eb66dedc229645727f5c075e6d395
DanSper <danilsperantsev@gmail.com>
2024-03-19 21:08:51 +0400

    add clickable on wizard

--- js/setup.js
+++ js/setup.js
@@ -55,3 +55,65 @@ for (var i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+
+// Задание на обработчики событий
+
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click',  openPopup);
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function() {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+var setupPlayer = document.querySelector('.setup-player');
+var wizardCoat = setupPlayer.querySelector('.wizard-coat');
+var wizardEyes = setupPlayer.querySelector('.wizard-eyes');
+var setupFireballWrap = document.querySelector('.setup-fireball-wrap');
+
+wizardCoat.addEventListener('click', function() {
+	wizardCoat.style.fill = getRandomItemFromArray(COAT_COLOR);
+});
+
+wizardEyes.addEventListener('click', function() {
+	wizardEyes.style.fill = getRandomItemFromArray(EYES_COLOR);
+});
+
+setupFireballWrap.addEventListener('click', function() {
+	setupFireballWrap.style.background = getRandomItemFromArray(EYES_COLOR);
+});
